09131efe3dc3c6bd433e6d1f814f5060e2d5863d
Volodymyr Romanenko <romanonebe@gmail.com>
2022-06-12 23:29:56 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,24 @@
 /*
 Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
 
-<div id="counter">
-  <button type="button" data-action="decrement">-1</button>
-  <span id="value">0</span>
-  <button type="button" data-action="increment">+1</button>
-</div>
 Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 Оновлюй інтерфейс новим значенням змінної counterValue.
 
 
 */
+const counterValue = document.querySelector("#value");
+console.log(counterValue.textContent);
+
+const decrementEl = document.querySelector('[data-action="decrement"]');
+
+decrementEl.addEventListener("click", () => {
+  counterValue.textContent -= 1;
+  console.log(counterValue.textContent);
+});
+const incrementEl = document.querySelector('[data-action="increment"]');
+
+incrementEl.addEventListener("click", () => {
+  counterValue.textContent = Number(counterValue.textContent) + 1;
+  console.log(counterValue.textContent);
+});
